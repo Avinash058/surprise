ca9e73b97bc9854f78cec11cb06c7ad481fe02c9
Avinash058 <64769831+Avinash058@users.noreply.github.com>
2021-04-28 11:55:54 +0530

    Add files via upload

---
+++ message/js/index.js
@@ -0,0 +1,22 @@
+this.$dom = {};
+this.$dom.animatedText = document.createElement('div');
+this.text = "A Very Happy Birthday to you. You are a special person who brought so much joy into my heart. I am thankful for every moment we spend together, and I wish our happiness never ends";  // MESSAGE AREA , THIS CAN BE CHANGED FROM YOUR MESSAGE :)
+this.$dom.container = document.getElementById('container');
+this.$dom.container.appendChild(this.$dom.animatedText);
+animateText();
+function animateText(){
+	var arrayOfLetters = this.text.split("");
+	var animatedSpan = [];
+	arrayOfLetters.forEach(function(item){
+		var span = document.createElement('span');
+		span.innerHTML = item;
+		animatedSpan.push(span);
+		span.style.opacity = 0;
+		span.style.color = 'white';
+		this.$dom.animatedText.appendChild(span);
+	});
+	TweenMax.staggerTo(animatedSpan, .2,{
+		opacity : 1,
+		delay : 2
+	},0.05);
+}
